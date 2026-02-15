e7fdd7e45e19aee28a0a8e9c93604ddb00ee29b5
joelmo <heitor.juliani19@gmail.com>
2026-02-15 01:17:00 +0000

    version 0.0.2 - woking

---
+++ src/js/apps.js
@@ -0,0 +1,11 @@
+Apps.example = {
+    title: 'Example App',
+    icon: 'fa-solid fa-star',
+    getContent() {
+        return `
+            <div style="padding: 20px;">
+                <p>Configure seus aplicativos aqui</p>
+            </div>
+        `;
+    }
+};
